3b1cb53e9bc95b2a30e0ecaea9a2e8107780d293
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-01-10 12:36:00 +0300

    Encrypting Password and username in the application.properties

--- EmployeeManagement/src/main/java/com/employeemanagement/employeemanagement/EmployeeManagementApplication.java
+++ EmployeeManagement/src/main/java/com/employeemanagement/employeemanagement/EmployeeManagementApplication.java
@@ -1,9 +1,11 @@
 package com.employeemanagement.employeemanagement;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class EmployeeManagementApplication {
 
     public static void main(String[] args) {
